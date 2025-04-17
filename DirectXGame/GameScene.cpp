a8149f677e70f78d265e9d49ff05df5750e6bdcb
onukiyu <k023g0017@g.neec.ac.jp>
2025-04-17 12:13:08 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,23 @@
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+// 初期化
+void GameScene::Initialize() {
+
+
+
+}
+// 更新
+void GameScene::Update() {
+
+
+
+}
+// 描画
+void GameScene::Draw() {
+
+
+
+}
+
