318e0047b142364caf29a7059a5bd20509d781da
onukiyu <k023g0017@g.neec.ac.jp>
2025-05-16 11:25:20 +0900

    02_02 完了

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -1,5 +1,7 @@
 #include "Particle.h"
 
+using namespace MathUtility;
+
 void Particle::Initialize(Model* model) {
 
 	// NULLポインタチェック
@@ -9,17 +11,29 @@ void Particle::Initialize(Model* model) {
 
 	worldTtansform_.Initialize();
 
+	// 色の設定
+	objectColor_.Initialize();
+	color_ = {1, 1, 0, 1};
 }
 
 void Particle::Update() {
 
+	// 移動
+	worldTtansform_.translation_ += {0.0f, 0.1f, 0.0f};
+
 	// 行列を定数バッファに転送
-	worldTtansform_.TransferMatrix();
+	//worldTtansform_.TransferMatrix();
+
+	// 行列を更新
+	worldTtansform_.UpdateMatrix();
+
+	// 色変更オブジェクトに色の数値を設定する
+	objectColor_.SetColor(color_);
 }
 
 void Particle::Draw(Camera& camera) {
 
 	//3Dモデルを描画
-	model_->Draw(worldTtansform_, camera);
+	model_->Draw(worldTtansform_, camera, &objectColor_);
 	
 }
--- DirectXGame/Particle.h
+++ DirectXGame/Particle.h
@@ -24,5 +24,8 @@ private:
 	// モデル
 	Model* model_ = nullptr;
 
-	
+	// 色変更オブジェクト
+	ObjectColor objectColor_;
+	// 色の数値
+	Vector4 color_;
 };
